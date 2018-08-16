9a97658993e7e64f877b0b6bbfdf5062f50d8fd3
Shanya <Shanya.williams24816@gmail.com>
2018-08-16 16:04:33 -0700

    08/16/18

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-// List of gifs from document
-
-/*
-1 'https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif',
-2 'https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif',
-3 'https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif',
-4 'https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif',
-5 'https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif',
-6 'https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif',
-7 'https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif',
-8 'https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif',
-9 'https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif',
-10 'https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif',
-
-*/
+var iframeElement = document.querySelector("#song-iframe");
 
 var gifs =[
-'url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)',
-'url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)',
-'url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)',
-'url(https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif)',
-'url(https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif)',
-'url(https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif)',
-'url(https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif)',
-'url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)',
-'url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)',
-'url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)'
+'url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)',//1
+'url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)',//2
+'url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)',//3
+'url(https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif)',//4
+'url(https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif)',//5
+'url(https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif)',//6
+'url(https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif)',//7
+'url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)',//8
+'url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)',//9
+'url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)'//10
 ]
 
-var currentSong = 0
-var songs = ['https://open.spotify.com/embed/track/6DCZcSspjsKoFjzjrWoCdn', ]
-// those are variables selecting main elements on the page
+var currentSong = 3;
+var songs = [
+		'spotify:track:3GCdLUSnKSMJhs4Tj6CV3s',
+		'spotify:track:7KXjTSCq5nL1LoYtL7XAwS',
+		'spotify:track:6tZ3b7ik1QDXFAZlXib0YZ',
+		'spotify:track:3GCdLUSnKSMJhs4Tj6CV3s',
+		'spotify:track:0KrmvQ1HsaWgeWSh8Cdb0o',
+ ]
 
 var resultBox = document.getElementById('result')
 var result2Box = document.getElementById('result')
@@ -45,27 +36,20 @@ var yrScore = document.getElementById('yr-score')
 var gifPanelBox = document.getElementById('gif-panel')
 var resultButton = document.getElementById('result-button')
 var nextSongButton = document.getElementById('nextbutton')
+var SongName = document.getElementById('nameofsong')
 
-// this is how you replace the image from the background
-// mainImage.style.backgroundImage = "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)"
-
-// this is how we hide the resultBox element
 resultBox.hidden = true;
 nextSongButton.hidden = true;
 
+setAlbumCover(songs[0], mainImage)
+iframeElement.src = getSpotifyScr(songs[0])
 
-// this is how you create a function
 function testClickFunction(){
   resultBox.hidden = true;
 }
 
-
-// Activity today will be
-
-// 1. Create a variable to store the user selected Rating  userRating
 var userRating = 0;
-// 2. Create 10 different functions to update the userRating score
-// 3. Add the functions to the correpondent HTML elements, using the onclick="functionName()" attribute
+
 function firstbuttonClickFunction(){
   mainImage.style.backgroundImage = "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)"
   userRating = 1;
@@ -123,11 +107,12 @@ function button10ClickFunction(){
 
 function nextSong(){
 	gifPanelBox.hidden = false;
+	resultBox.hidden = true;
+	nextSongButton.hidden = true;
+	setAlbumCover(songs[currentSong + 1], mainImage)
 
 }
 
-
-// 4. Create a function to process the click on Compare button
 function compareCLickFunction(){
   gifPanelBox.hidden = true;
   youScore.innerHTML = userRating;
@@ -137,56 +122,55 @@ function compareCLickFunction(){
 
   if(userRating === 1) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 2) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 3) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 4) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 5) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 6) {
-   youImage.style.backgroundImage = "url(https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 7) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 8) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 9) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
   if(userRating === 10) {
    youImage.style.backgroundImage = "url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)"
-   yrImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
-   spotifyImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)"
   }
 
 }
-// 5. The function should, hide the element voteBox and show resultBox
-// 5.1 Show User score replacing youScore.innerHTML = content
-// 5.2. Change youImage with appropriated Gif image
+
+
+
+
+function getSpotifyScr(song) {
+	const songCode = song.split(':')[2];
+	return `https://open.spotify.com/embed/track/${songCode}`;
+}
+
+
+async function setAlbumCover(song, element) {
+  var aa = await getAlbumCover(song);
+  element.style.backgroundImage = `url(${aa})`;
+}
+
+async function getAlbumCover(song) {
+  //return the Image for a specific song ID
+  const songCode = song.split(':')[2];
+  const data = await fetch(`https://cors-anywhere.herokuapp.com/https://embed.spotify.com/oembed?url=http://open.spotify.com/track/${songCode}`)
+    .then(r => r.json());
+  return data.thumbnail_url;
+}
+
