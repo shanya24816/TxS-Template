0b32e5733337fa2ce2d9a05e93e6f004f27a0962
Shanya <Shanya.williams24816@gmail.com>
2018-08-20 17:55:05 -0700

    08/20/18

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var gifs =[
 'url(https://media.giphy.com/media/EMA2GQg31g436/giphy.gif)',//3
 'url(https://media.giphy.com/media/ScZzMlETdv9mg/giphy.gif)',//4
 'url(https://media.giphy.com/media/NV5Pi952EDymZvh8J1/giphy.gif)',//5
-'url(https://media.giphy.com/media/pkmRPHqVENUOI/giphy.gif)',//6
+'url(https://media.giphy.com/media/1rf4V8kgTLvva/giphy.gif)',//6
 'url(https://media.giphy.com/media/l46CyxkMBFwHlJ3kk/giphy.gif)',//7
 'url(https://media.giphy.com/media/pa37AAGzKXoek/giphy.gif)',//8
 'url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)',//9
@@ -14,25 +14,68 @@ var gifs =[
 ]
 
 var currentSong = 0;
-var songs = [
-		'spotify:track:3GCdLUSnKSMJhs4Tj6CV3s',//danceability": 0.698
-		'spotify:track:7KXjTSCq5nL1LoYtL7XAwS',//danceability": 0.908
-		'spotify:track:6tZ3b7ik1QDXFAZlXib0YZ',//danceability": 0.829
-		'spotify:track:3GCdLUSnKSMJhs4Tj6CV3s',//danceability": 0.698
-		'spotify:track:0KrmvQ1HsaWgeWSh8Cdb0o',//danceability": 0.547
- ]
 
- var spotifyScores = [
- 			7,
- 			9,
- 			8,
- 			7,
- 			5
- ]
+ // var spotifyScores = [
+ // 			7,
+ // 			9,
+ // 			8,
+ // 			5
+ // ]
 
+var songTitle = [
+  'All the Stars',
+  'Humble',
+  'Kevins Heart',
+  'Doing the Sponge'
+]
 
+var artist = [
+  'Kendrick Lamar, SZA',
+  'Kendrick Lamar',
+  'J Cole',
+  'Spongebob Squarepants'
+]
 
+var songData = [
+  {
 
+    songTitle: "All the Stars",
+    artist: "Kendrick Lamar, SZA",
+    spotifyScore: 7,
+    songURI: 'spotify:track:3GCdLUSnKSMJhs4Tj6CV3s',//danceability": 0.698
+    userRating: null,
+    backgroundColor: "green",
+  },
+
+  {
+    songTitle: "Humble",
+    artist: "Kendrick Lamar",
+    spotifyScore: 9,
+    songURI: 'spotify:track:7KXjTSCq5nL1LoYtL7XAwS',//danceability": 0.908
+    userRating: null,
+    backgroundColor: "blue",
+  },
+
+  {
+  songTitle: "Kevins Heart",
+    artist: "J Cole",
+    spotifyScore: 8,
+    songURI: 'spotify:track:6tZ3b7ik1QDXFAZlXib0YZ',//danceability": 0.829
+    userRating: null,
+    backgroundColor: "orange",
+  },
+
+  {
+    songTitle: "Doing the Sponge",
+    artistName: "Spongebob Squarepants",
+    spotifyScore: 5,
+    songURI: 'spotify:track:0KrmvQ1HsaWgeWSh8Cdb0o',//danceability": 0.547
+    userRating: null,
+    backgroundColor: "purple",
+
+  }
+
+]
 
 var resultBox = document.getElementById('result')
 var result2Box = document.getElementById('result')
@@ -53,8 +96,8 @@ var SongName = document.getElementById('nameofsong')
 resultBox.hidden = true;
 nextSongButton.hidden = true;
 
-setAlbumCover(songs[0], mainImage)
-iframeElement.src = getSpotifyScr(songs[0])
+setAlbumCover(songData[0].songURI, mainImage)
+iframeElement.src = getSpotifyScr(songData[0].songURI)
 
 function testClickFunction(){
   resultBox.hidden = true;
@@ -92,12 +135,12 @@ function button5ClickFunction(){
   resultButton.hidden = false;
 }
 function button6ClickFunction(){
-  mainImage.style.backgroundImage = "url(https://media.giphy.com/media/pkmRPHqVENUOI/giphy.gif)"
+  mainImage.style.backgroundImage = "url(https://media.giphy.com/media/l46CyxkMBFwHlJ3kk/giphy.gif)"
   userRating = 6;
   resultButton.hidden = false;
 }
 function button7ClickFunction(){
-  mainImage.style.backgroundImage = "url(https://media.giphy.com/media/l46CyxkMBFwHlJ3kk/giphy.gif)"
+  mainImage.style.backgroundImage = "url(https://media.giphy.com/media/1rf4V8kgTLvva/giphy.gif)"
   userRating = 7;
   resultButton.hidden = false;
 }
@@ -122,8 +165,8 @@ function nextSong(){
 	resultBox.hidden = true;
 	nextSongButton.hidden = true;
 	currentSong = currentSong +1
-	setAlbumCover(songs[currentSong], mainImage)
-	iframeElement.src = getSpotifyScr(songs[currentSong])
+	setAlbumCover(songData[currentSong].songURI, mainImage)
+	iframeElement.src = getSpotifyScr(songData[currentSong].songURI)
 }
 
 function compareCLickFunction(){
@@ -131,11 +174,11 @@ function compareCLickFunction(){
   youScore.innerHTML = userRating;
   resultBox.hidden = false;
   nextSongButton.hidden = false;
-  spotifyScore.innerHTML = spotifyScores[currentSong];
+  spotifyScore.innerHTML = songData[currentSong].spotifyScore;
 
  youImage.style.backgroundImage = gifs[userRating - 1];
 
- spotifyImage.style.backgroundImage = gifs[spotifyScores[currentSong]-1];
+ spotifyImage.style.backgroundImage = gifs[songData[currentSong].spotifyScore-1];
 }
 
 function getSpotifyScr(song) {
@@ -156,5 +199,3 @@ async function getAlbumCover(song) {
     .then(r => r.json());
   return data.thumbnail_url;
 }
-
-
